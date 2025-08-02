a9e7ab1073426c8750e46215d05d518d5d9a96dd
Chris Kampolis <chris.kampolis@outlook.com>
2025-08-02 10:01:58 +0300

    feat: added availability on MenuItem

--- restaurant/models.py
+++ restaurant/models.py
@@ -16,16 +16,12 @@ class User(AbstractUser):
 class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    # availability = models.BooleanField(default=True)                  # FIX !!!!!!
+    availability = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
 
-# class Reservation(models.Model):
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     seat_count = models.IntegerField()
-#     reservation_time = models.DateField(auto_now=True)
+
 
 
     
