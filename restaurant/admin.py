881c6f7d6ff8f719657a25442d53c615230e5b23
Chris Kampolis <chris.kampolis@outlook.com>
2025-08-05 22:10:33 +0300

    feat: added Order and OrderItem in admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User, MenuItem
+from .models import User, MenuItem, Order, OrderItem
 
-# Register your models here.
 admin.site.register(User)
 admin.site.register(MenuItem)
-# admin.site.register(Reservation)
+admin.site.register(Order)
+admin.site.register(OrderItem)
