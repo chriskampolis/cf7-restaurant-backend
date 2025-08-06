1aa6fe90e766e3d4ecdaa618a6a4f50efe765ef1
Chris Kampolis <chris.kampolis@outlook.com>
2025-08-06 17:27:56 +0300

    feat: created forms

---
+++ restaurant/forms.py
@@ -0,0 +1,13 @@
+# from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name", "role")
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name", "role", "is_active", "is_staff", "is_superuser")
