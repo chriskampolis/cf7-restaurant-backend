9acee5b49f138640fee8e688ed4bd32a404525ac
Chris Kampolis <chris.kampolis@outlook.com>
2025-09-03 16:34:01 +0300

    feat: completed-orders created

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,12 @@
-# from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, MenuItemViewSet, OrderViewSet
+from .views import CompletedOrdersView, UserViewSet, MenuItemViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'menu-items', MenuItemViewSet)
 router.register(r'orders', OrderViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path("completed-orders/", CompletedOrdersView.as_view(), name="completed-orders"),
+]
